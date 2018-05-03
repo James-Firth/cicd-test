416636976e5e66425a1ef4669ae64ca55f976d9b
James Firth <hello@james-firth.com>
2018-05-03 11:00:29 -0500

    Uses Build Stages now. Using Travis not CircleCI

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router(); // eslint-disable-line new-cap
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
